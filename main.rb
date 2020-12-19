3b450cb41762ef913d0f78b5962ac8c117206209
OnePhraseBlues <jdmgalindo@gmail.com>
2020-12-19 04:38:17 +0000

    I think it needs to be an array actually.

--- main.rb
+++ main.rb
@@ -1,19 +1,17 @@
 class GameBoard
 
 =begin
-Under the hood, the game board will be an array of 9 booleans.
-True will represent X. False will represent O. nil will represent available space.
-Not exactly sure how testing for nil works but we're about to find out!
+Under the hood, the game board will be a hash with 9 keys. If the key exists, the logic 
+will determine that the move is illegal. Still need to figure out a way to test for the
+win condition
 =end
 
   def initialize
-    @board = []
-    9.times do
-      @board.push(nil)
-    end
+    @board = {}
   end
   
   def display(board_array)
+    #we can still take an array as input but contain whitespace strings if vacant
     first_row = "#{board_array[0]} / #{board_array[1]} / #{board_array[2]}"
     second_row = "#{board_array[3]} / #{board_array[4]} / #{board_array[5]}"
     third_row = "#{board_array[6]} / #{board_array[7]} / #{board_array[8]}"
@@ -22,27 +20,9 @@ Not exactly sure how testing for nil works but we're about to find out!
     puts third_row
   end
 
-
   def fill_space(player_move) #player_move will likely be a hash containing the player and the space
-    case player_move["space"]
-    when 7
-      @board[0] = player_move["play"]
-    when 8
-      @board[1] = player_move["play"]
-    when 9
-      @board[2] = player_move["play"]
-    when 4
-      @board[3] = player_move["play"]
-    when 5
-      @board[4] = player_move["play"]
-    when 6
-      @board[5] = player_move["play"]
-    when 1
-      @board[6] = player_move["play"]
-    when 2
-      @board[7] = player_move["play"]
-    when 3
-      @board[8] = player_move["play"]
-    end
+    @board[player_move.keys[0]] = player_move["play"]
   end
 
+  def win_test
+    if()
